263f632fdbd920a1d46c3df844dda1e6575feb1f
mohammed-hamdi26 <mohammedhamdi262003@gmail.com>
2025-11-15 21:20:27 +0200

    feat: add support center functionality with ticket retrieval and display

--- components/ui app/AnswerBox.jsx
+++ components/ui app/AnswerBox.jsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import imagePerson from "../../public/images/dashboard/avatar.jpg";
 import { Input } from "../ui/input";
-import { Link, Send } from "lucide-react";
+import { Link, Send, X } from "lucide-react";
 import { Button } from "../ui/button";
-function AnswerBox({ chat }) {
+function AnswerBox({ chat, onClose }) {
   return (
-    <div className="bg-[#10131D] w-[702px] h-[500px] rounded-3xl flex flex-col overflow-hidden ">
-      <div className="px-11 pt-14 overflow-x-scroll space-y-8 flex-1">
+    <div className="bg-dashboard-box dark:bg-[#10131D]  mx-auto  sm:w-[702px] h-[500px] rounded-3xl flex flex-col overflow-hidden ">
+      <div className="flex items-center justify-end p-4">
+        <X
+          size={40}
+          className="cursor-pointer text-green-primary hover:text-green-primary/70 dark:text-white dark:hover:text-white/70 transition-colors duration-300 "
+          onClick={() => {
+            onClose();
+          }}
+        />
+      </div>
+      <div className="px-4 md:px-11 pt-0 overflow-y-auto space-y-8 flex-1">
         {Array.from({ length: 2 }, (_, index) => (
           <PersonMessage
             info={{
@@ -44,17 +53,19 @@ function PersonMessage({ info }) {
 function SendMessageInput() {
   return (
     <div className="bg-dashboard-box  dark:bg-[#0F1017] p-4">
-      <div className="flex items-center justify-between bg-[#10131D] py-4 px-2.5 gap-4 rounded-xl">
+      <div className="flex items-center justify-between bg-dashboard-box  dark:bg-[#10131D] py-4 px-2.5 gap-4 rounded-xl">
         <Input
-          className={"  border-none"}
+          className={
+            " text-[#10131D]  dark:bg-[#10131D]  shadow-none border-none focus:outline-none "
+          }
           type="text"
           placeholder="Type your message..."
         />
         <div className="flex items-center gap-3">
-          <Link className="cursor-pointer h-4 w-4" />
-          <span className="h-[20px] w-[1px] bg-gray-400"></span>
-          <Button className="bg-green-primary cursor-pointer size-9 rounded-full flex justify-center items-center ">
-            <Send />
+          {/* <Link className="cursor-pointer h-4 w-4" />
+          <span className="h-[20px] w-[1px] bg-gray-400"></span> */}
+          <Button className="bg-green-primary cursor-pointer size-9 rounded-full flex justify-center items-center hover:bg-green-primary/70 ">
+            <Send className="text-white" />
           </Button>
         </div>
       </div>
