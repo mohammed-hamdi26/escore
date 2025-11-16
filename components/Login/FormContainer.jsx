0f918e47e0d06e7c1aa6016506941b6ad363993f
mohammed-hamdi26 <mohammedhamdi262003@gmail.com>
2025-11-16 18:41:53 +0200

    feat: enhance login functionality and improve layout responsiveness

--- components/Login/FormContainer.jsx
+++ components/Login/FormContainer.jsx
@@ -3,27 +3,28 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import LoginFrom from "./LoginFrom";
 import UsersContainer from "./UsersContainer";
+import { login } from "@/app/[locale]/_Lib/actions";
+import toast from "react-hot-toast";
 const validationSchema = Yup.object({
-  username: Yup.string().required("Required"),
+  username: Yup.string().required("username is required"),
   // email: Yup.string().email("Invalid email address").required("Required"),
-  password: Yup.string().required("Required"),
-  role: Yup.string().required("Required"),
+  password: Yup.string().required("password is required"),
+  // role: Yup.string().required("Required"),
 });
 
 function FormContainer() {
   const formik = useFormik({
     initialValues: {
       username: "",
-      email: "",
+
       password: "",
-      role: "admin",
     },
     validationSchema: validationSchema,
-    onSubmit: async values => {
+    onSubmit: async (values) => {
       // console.log(values);
       try {
-        // await login(values);
-        // toast.success("Login successful");
+        await login(values);
+        toast.success("Login successful");
       } catch (error) {
         // // console.log(error);
         toast.error(error.message);
@@ -32,10 +33,10 @@ function FormContainer() {
   });
 
   return (
-    <div className="flex items-center justify-between w-full relative">
-      <div className="flex items-center gap-14">
-        <UsersContainer formik={formik} />
-        <h1 className="text-6xl font-bold z-10 ">
+    <div className="flex flex-col md:flex-row items-center justify-between w-full relative">
+      <div className="flex flex-col items-center gap-14">
+        {/* <UsersContainer formik={formik} /> */}
+        <h1 className="hidden md:block text-6xl font-bold z-10 ">
           Sign In to Go <br />
           to Dashboard
         </h1>
