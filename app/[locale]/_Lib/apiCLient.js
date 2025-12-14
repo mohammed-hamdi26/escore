269cb0485f649e5d7c84558ebef4c0496f5cd367
mohammed-hamdi26 <mohammedhamdi262003@gmail.com>
2025-12-14 02:22:12 +0200

    edit api client

--- app/[locale]/_Lib/apiCLient.js
+++ app/[locale]/_Lib/apiCLient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getSession, deleteSession } from "./session";
 
-const eapiClient = axios.create({
+const apiClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
